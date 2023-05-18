078dcc6f57d36d3de6f3126dcef6f3a763359f83
Demetris Roumis <roumis.d@gmail.com>
2023-05-18 23:33:29 +0200

    reorganize and structure repo

--- ephys/waveform/dev/load_waveforms.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-def load_waveform_templates(wfpath = './waveforms.csv'):
-    waveforms = pd.read_csv(wfpath, index_col = 'uid')
-    wf = waveforms.drop(['organoid'], axis = 1, inplace=False)
-    wf.columns = wf.columns.astype('int')
-    return wf
