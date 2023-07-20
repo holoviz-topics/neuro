ee11db719e12e0dfed64666b898367e2d124af4a
Ian Thomas <ianthomas23@gmail.com>
2023-07-20 10:40:32 +0100

    Only grab console messages for a single figure of a Bokeh document

--- benchmarks/benchmarks/base.py
+++ benchmarks/benchmarks/base.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from bokeh.models import Plot
 from bokeh.server.server import Server
 from playwright.sync_api import sync_playwright
 
@@ -16,15 +17,23 @@ class Base:
     def __init__(self):
         self._port = 5006
         self.render_count = -1
+        self._figure_id = None  # Unique ID of the figure to grab the console messages of.
 
     def _console_callback(self, msg: ConsoleMessage) -> None:
-        # Only supports a single Bokeh canvas so far.
-        args = msg.args
-        if len(args) == 3 and args[0].json_value() == "PlotView._actual_paint" and args[2].json_value() == "end":
-            self.render_count += 1
-            count = int(args[1].json_value())
-            if count != self.render_count:
-                raise RuntimeError(f"Mismatch in render count: {count} != {self.render_count}")
+        if self._figure_id is None or len(msg.args) != 4:
+            return
+
+        args = [arg.json_value() for arg in msg.args]
+
+        if args[0] == "PlotView._actual_paint" and args[1] == self._figure_id:
+            if args[3] == "start":
+                # TODO: need to handle start of render if want to time a single render.
+                pass
+            elif args[3] == "end":
+                self.render_count += 1
+                count = int(args[1].json_value())
+                if count != self.render_count:
+                    raise RuntimeError(f"Mismatch in render count: {count} != {self.render_count}")
 
     def _playwright_setup(self, bokeh_doc: Callable[[Document], None], catch_console: bool) -> None:
         # Playwright context manager needs to span multiple functions,
@@ -40,10 +49,19 @@ class Base:
         self.page = self._browser.new_page()
         self.page.goto(f"http://localhost:{self._port}/")
 
+        # Assume Bokeh document contains a single figure, and obtain its ID.
+        sessions = self._server.get_sessions()
+        if len(sessions) != 1:
+            raise RuntimeError(f"Expected a single session but have {len(sessions)}")
+        doc = sessions[0].document
+        # This raises an error if there is more than one figure in the Bokeh document.
+        self._figure_id = doc.select_one(dict(type=Plot)).id
+
         if catch_console:
             self.page.on("console", self._console_callback)
 
     def _playwright_teardown(self):
+        self._figure_id = None
         self._browser.close()
         self._server.stop()
         self._playwright_context_manager.__exit__(None, None, None)
