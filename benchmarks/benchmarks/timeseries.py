a0e32564fd0d843ed206cbbfa77c870bc3ab0ee6
Ian Thomas <ianthomas23@gmail.com>
2023-08-02 12:59:06 +0100

    Store render counts for multiple Bokeh figures

--- benchmarks/benchmarks/timeseries.py
+++ benchmarks/benchmarks/timeseries.py
@@ -44,12 +44,17 @@ class Timeseries(Base):
         bkapp_n = partial(bkapp, n=n, output_backend=output_backend)
         self.playwright_setup(bkapp_n)
 
+        # There is only a single Bokeh figure in each benchmark so store its ID here rather than
+        # in the benchmark itself.
+        self.figure_id = self.current_figure_id()
+
     def teardown(self, n: int, output_backend: str) -> None:
+        self.figure_id = None
         self.playwright_teardown()
 
     def time_values(self, n: int, output_backend: str) -> None:
         button = self.page.get_by_role("button", name="run")
-        start_render_count = self.render_count
+        start_render_count = self.render_count(self.figure_id)
         button.click()
-        while self.render_count == start_render_count:
+        while self.render_count(self.figure_id) == start_render_count:
             self.page.wait_for_timeout(1)
